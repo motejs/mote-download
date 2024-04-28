8b6be5468ea4795d19b9c5ab3bd72050388d182d
sunzhaowei <sunzhaowei@doba.com>
2024-04-28 20:54:03 +0800

    v0.2.0 去繁存简，更直接更简单

--- index.js
+++ index.js
@@ -2,44 +2,34 @@
 
 const { Command } = require("commander");
 const program = new Command();
-const chalk = require("chalk");
-const figlet = require("figlet");
+const create = require("./lib/create");
 
 program
   .name("mote-download")
-  .description(
-    "A simple cli for download template from github/gitlab/bitbucket"
-  )
-  .usage("<command> [option]")
-  .version(`v${require("./package.json").version}`);
+  .description("A simple cli for download template from github")
+  .usage("[options] [project-name]")
+  .version(`v${require("./package.json").version}`)
 
-program
-  .command("create [project-name]")
-  .description("create a new project from a template")
+  .option("--name <name>", "工程目录名称")
+  .option("-o, --owner <owner>", "模板库所属账号（Owner），会缓存到本地，再次输入会覆盖缓存的Owner")
+  .option("-t, --token <token>", "账号Token，如果是私有库需要提供，会缓存到本地，再次输入会覆盖缓存的Token")
+  .option("-k, --keywords <keywords>", "过滤模板库的名称关键字")
+  .option("-r, --repo <repo>", "指定模板库名称")
+  .option("-r, --tag <tag>", "指定模板库Tag版本")
+
   .option("-f, --force", "如果目录存在，强制覆盖")
-  .option("-c, --clone", "用'git clone'下载库")
-  .option("-p, --private", "下载公开和私有库")
-  .option("-lc, --lastConfig", "使用上次配置")
-  .action((name, options) => {
-      require("./lib/create.js")(name, options); 
-  });
+  .option("-c, --clone", "指定用'git clone'方式下载库");
 
-program.on("--help", () => {
-  // 使用 figlet 绘制 Logo
-  // console.log(
-  //   "\r\n" +
-  //     figlet.textSync("mote-DL", {
-  //       font: "Ghost",
-  //       horizontalLayout: "default",
-  //       verticalLayout: "default",
-  //       width: 80,
-  //       whitespaceBreak: true,
-  //     })
-  // );
-  // 新增说明信息
-  console.log(
-    `\r\nRun ${chalk.cyan(`mote-download <command> --help`)} show details\r\n`
-  );
-});
+  // .command("create [project-name]")
+  // .description("create a new project from a template")
 
-program.parse();
+  // .action((name, options) => {
+  //   require("./lib/create.js")(name, options);
+  // });
+
+program.parse(process.argv);
+
+const options = program.opts();
+// console.log("options", options);
+
+create(options)
